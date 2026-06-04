871629f54026705461ea5cc813df564fe1bd7662
Aki <achi.roziiz@gmail.com>
2026-06-04 19:11:50 +0200

    feat: add bottom bar and placeholders for screens not created yet.

--- app/src/main/java/com/example/matchmovie/enumentity/Screen.kt
+++ app/src/main/java/com/example/matchmovie/enumentity/Screen.kt
@@ -3,10 +3,15 @@ package com.example.matchmovie.enumentity
 enum class Screen {
     LoginPage,
 
+    HomeScreen,
+
+    ProfileScreen,
+
+    MyListScreen,
+
     // Contiene la barra di ricerca + la lista dei risultati ottenuti (sottoforma di lista)
     SearchScreen,
 
     // Contiene i dettagli di un film selezionato dall'utente, ottenuto dalla ricerca
     FilmDetailsScreen,
-    MyFilmScreen,
 }
